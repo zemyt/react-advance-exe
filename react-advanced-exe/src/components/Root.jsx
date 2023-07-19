a32638ee491390e3c1d8fe2f63eda60ed6567ffa
Zemyt <tijme_@hotmail.com>
2023-07-19 18:20:13 +0200

    Add Footer

--- react-advanced-exe/src/components/Root.jsx
+++ react-advanced-exe/src/components/Root.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navigation } from "./Navigation";
 import { Box } from "@chakra-ui/react";
+import { Footer } from "./Footer";
 
 export const Root = () => {
   return (
     <Box>
       <Navigation />
       <Outlet />
+      <Footer />
     </Box>
   );
 };
